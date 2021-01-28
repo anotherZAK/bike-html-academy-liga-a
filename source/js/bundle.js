e1a2cfbe6edc7de32e21b7411c64847b741a4c88
Pavel <pasha.krupko@yandex.ru>
2021-01-28 13:34:39 +0300

    Корректирует стили мобильного меню на соответствие ТЗ

--- source/js/bundle.js
+++ source/js/bundle.js
@@ -7,8 +7,13 @@
 
 (function () {
   var burgerToggle = document.querySelector('.main-nav');
+  var pageHeader = document.querySelector('.page-header');
+  var bgContainer = document.querySelector('.background-container');
 
   burgerToggle.classList.remove('main-nav--nojs');
+  bgContainer.classList.remove('background-container--nojs');
+  bgContainer.classList.add('background-container--header');
+  pageHeader.style.marginBottom = 70 + 'px';
 
   burgerToggle.addEventListener('click', function () {
     burgerToggle.classList.toggle('main-nav--opened');
