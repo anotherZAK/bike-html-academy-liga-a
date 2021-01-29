1d4c626377454e8a328acda8e66f024731a611e5
Pavel <pasha.krupko@yandex.ru>
2021-01-29 10:05:33 +0300

    Вносит правки по замечаниям в баг-листе

--- source/js/bundle.js
+++ source/js/bundle.js
@@ -29,12 +29,12 @@
 
 
 (function () {
-  var phonePattern = new RegExp('[\+][0-9]{1}[\(]\[0-9]{3}[\)][0-9]{3}[\-][0-9]{2}[\-][0-9]{2}');
+  var phonePattern = new RegExp('[\+][0-9]{11}|[0-9]{11}');
   var userTel = document.querySelector('#bike-choice-user-phone-id');
 
   userTel.addEventListener('input', function () {
     if (!phonePattern.test(userTel.value)) {
-      userTel.setCustomValidity('Введите телефон в формате: +X(XXX)-XXX-XX-XX');
+      userTel.setCustomValidity('Введите телефон в формате: +7XXXXXXXXXX или 8XXXXXXXXXX');
     } else {
       userTel.setCustomValidity('');
     }
